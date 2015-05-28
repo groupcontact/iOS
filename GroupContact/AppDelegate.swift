58da814ff607a35297cfac81bbdd81b93e642c32
zhouhaibing089 <zhouhaibing089@gmail.com>
2015-05-28 09:50:53 +0800

    Structure Refine

--- GroupContact/AppDelegate.swift
+++ GroupContact/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  GroupContact
-//
-//  Created by Haibing Zhou on 4/26/15.
-//  Copyright (c) 2015 Haibing Zhou. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
@@ -14,14 +6,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // 获取UIStoryboard实例
+        let uiStoryboard = UIStoryboard.init(name: "Main", bundle: nil)
+        // 获取NSUserDefaults实例
+        let defaults = NSUserDefaults.standardUserDefaults()
         // 读取配置，用户是否已经登录
-        let uid = NSUserDefaults.standardUserDefaults().integerForKey("uid")
-        if uid == 0 {
+        let uid = defaults.valueForKey("uid") as? NSNumber
+        if uid == nil {
             // 如果未登录, 则进入登录/注册页面
-            self.window?.rootViewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Auth") as? UIViewController
+            window?.rootViewController = uiStoryboard.instantiateViewControllerWithIdentifier("Auth") as? UIViewController
         } else {
+            // 初始化数据
+            Var.initVar()
             // 如果已登录, 则进入主页面
-            self.window?.rootViewController = UIStoryboard.init(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("Main") as? UIViewController
+            window?.rootViewController = uiStoryboard.instantiateViewControllerWithIdentifier("Main") as? UIViewController
+            // 并初始化数据
+            Var.uid = uid!.unsignedLongLongValue
         }
         return true
     }
--- GroupContact/Var.swift
+++ GroupContact/Var.swift
@@ -1,21 +1,166 @@
-//
-//  Var.swift
-//  GroupContact
-//
-//  Created by Haibing Zhou on 5/2/15.
-//  Copyright (c) 2015 Haibing Zhou. All rights reserved.
-//
-
 import Foundation
 
+/*
+ * 1. 存储程序运行时的变量, 避免经常去读NSUserDefaults.
+ * 2. 存储程序运行时的数据, 避免经常去读文件
+ *
+ * 此处存储的变量的值必须时刻保持与NSUserDefaults中的值一致
+ * 此处存储的数据的值必须时刻保持与文件中的值一致
+ */
 struct Var {
     
-    static var uid = UInt64(5)
+    // 程式的Key/Value存储区域
+    private static let userDefaults = NSUserDefaults.standardUserDefaults()
+    // 文件管理器
+    private static let fileManager = NSFileManager.defaultManager()
     
-    static var name: String = "周海兵"
+    // 用户的id
+    static var uid = UInt64(0) {
+        didSet {
+            // 写入NSUserDefaults
+            if oldValue != uid {
+                userDefaults.setValue(NSNumber(unsignedLongLong: uid), forKey: "uid")
+                userDefaults.synchronize()
+            }
+        }
+    }
     
-    static var password: String = "199311"
+    // 用户的姓名
+    static var name: String = "" {
+        didSet {
+            // 写入NSUserDefaults
+            if oldValue != name {
+                userDefaults.setValue(name, forKey: "name")
+                userDefaults.synchronize()
+            }
+        }
+    }
     
-    static var user = UserAO(uid: Var.uid, name: Var.name, phone: "18205082827", ext: "{\"email\": \"877498144@qq.com\", \"wechat\": \"zhouhaibing089\"}")
+    // 用户的密码
+    static var password: String = "" {
+        didSet {
+            // 写入NSUserDefaults
+            if oldValue != password {
+                userDefaults.setValue(password, forKey: "password")
+                userDefaults.synchronize()
+            }
+        }
+    }
     
+    // 用户的完整信息
+    static var user: UserAO? {
+        didSet {
+            // 第一次从文件初始化
+            if oldValue == nil {
+                return
+            }
+            // 写入缓存文件
+            let urls = fileManager.URLsForDirectory(NSSearchPathDirectory.CachesDirectory,
+                inDomains: NSSearchPathDomainMask.UserDomainMask)
+            if urls.count > 0 {
+                let cachePath = (urls[0] as! NSURL).URLString
+                let path = cachePath + "profile.s"
+                var error: NSError?
+                // 将user转换成JSON字符串
+                let userJSON = "HelloWorld"
+                let succeeded = userJSON.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding, error: &error)
+                if !succeeded {
+                    if let theError = error {
+                        println("Could not save user to file. Error = \(theError)")
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    // 好友列表信息
+    static var friends: [UserAO]? {
+        didSet {
+            // 第一次从文件初始化
+            if oldValue == nil {
+                return
+            }
+            // 写入缓存文件
+            let urls = fileManager.URLsForDirectory(NSSearchPathDirectory.CachesDirectory,
+                inDomains: NSSearchPathDomainMask.UserDomainMask)
+            if urls.count > 0 {
+                let cachePath = (urls[0] as! NSURL).URLString
+                let path = cachePath + "friends.s"
+                var error: NSError?
+                // 将friends转换成JSON字符串
+                let friendsJSON = "HelloWorld"
+                let succeeded = friendsJSON.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding, error: &error)
+                if !succeeded {
+                    if let theError = error {
+                        println("Could not save friends to file. Error = \(theError)")
+                    }
+                }
+            }
+
+        }
+    }
+    
+    // 群组列表信息
+    static var groups: [GroupAO]? {
+        didSet {
+            // 第一次从文件初始化
+            if oldValue == nil {
+                return
+            }
+            // 写入缓存文件
+            let urls = fileManager.URLsForDirectory(NSSearchPathDirectory.CachesDirectory,
+                inDomains: NSSearchPathDomainMask.UserDomainMask)
+            if urls.count > 0 {
+                let cachePath = (urls[0] as! NSURL).URLString
+                let path = cachePath + "groups.s"
+                var error: NSError?
+                // 将groups转换成JSON字符串
+                let groupsJSON = "HelloWorld"
+                let succeeded = groupsJSON.writeToFile(path, atomically: true, encoding: NSUTF8StringEncoding, error: &error)
+                if !succeeded {
+                    if let theError = error {
+                        println("Could not save groups to file. Error = \(theError)")
+                    }
+                }
+                
+            }
+        }
+    }
+    
+    // 初始化数据, 包括user, friends和groups
+    static func initVar() {
+        let urls = fileManager.URLsForDirectory(NSSearchPathDirectory.CachesDirectory,
+            inDomains: NSSearchPathDomainMask.UserDomainMask)
+        if urls.count > 0 {
+            let cachePath = (urls[0] as! NSURL).URLString
+            // 读取user信息
+            let userPath = cachePath + "profile.s"
+            if let userJSON = NSString(contentsOfFile: userPath, encoding: NSUTF8StringEncoding
+                , error: nil) as? String {
+                // 将其转换成UserAO对象
+            } else {
+                // 默认是一个无手机号, 无扩展信息的用户
+                user = UserAO(uid: Var.uid, name: Var.name, phone: "", ext: "{}")
+            }
+            // 读取friends信息
+            let friendsPath = cachePath + "friends.s"
+            if let friendsJSON = NSString(contentsOfFile: friendsPath, encoding: NSUTF8StringEncoding
+                , error: nil) as? String {
+                // 将其转换成[UserAO]对象
+            } else {
+                // 默认是空的好友列表
+                friends = [UserAO]()
+            }
+            // 读取groups信息
+            let groupsPath = cachePath + "groups.s"
+            if let groupsJSON = NSString(contentsOfFile: groupsPath, encoding: NSUTF8StringEncoding
+                , error: nil) as? String {
+                // 将其转换成[GroupAO]对象
+            } else {
+                // 默认是空的群组列表
+                groups = [GroupAO]()
+            }
+        }
+    }
 }
